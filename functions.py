3f8fd33a044ca8c96c37983efc0007dc97f072e1
nchhaba <nihaar17000@gmail.com>
2024-12-09 17:58:05 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -30,10 +30,45 @@ class Functions:
 
 
     @staticmethod
-    
+    def currency_raw(value):
+        """
+        Converts a dollar-formatted value to a float.
+
+        Parameters:
+        - value (str): Value to be converted.
+
+        Returns:
+        - float: Converted value.
+
+        Example:
+        >>> Functions.currency_raw('$1,250.50')
+        1250.5
+
+        >>> Functions.currency_raw('$1,000.50')
+        1000.5
+        """
+        return float(value.replace('$', '').replace(',', ''))
 
     @staticmethod
-    
+    def export(self,initial_balance, current_balance, expenses):
+        """
+        Transfers the user to an export screen for exporting files.
+
+        Parameters:
+        - initial_balance (float): Initial balance.
+        - current_balance (float): Current balance.
+        - expenses (list): List of expenses.
+
+        Example:
+        >>> Functions.export(3000.50, 1500.50, [('Rent', 1500)])
+        >>> export_screen()
+        - Displays the export screen for exporting files.
+        - Exports the values to the export file.
+        - Closes the export screen.
+        - Returns to the main screen.
+        """
+        self.export_values(initial_balance, current_balance, expenses)
+        self.export_screen()
 
     @staticmethod
     def importer(self):
